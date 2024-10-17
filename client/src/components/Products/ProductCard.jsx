6bebd614ba703281aa48455d46575fa6a1107ab9
CodeCantalope <deedoleedo@gmail.com>
2024-10-17 10:13:34 +0300

    first commit

--- client/src/components/Products/ProductCard.jsx
+++ client/src/components/Products/ProductCard.jsx
@@ -3,31 +3,61 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 export default class ProductCard extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
       product: props.product,
     };
   }
+
+  checkStock = (var1, var2) => {
+    return this.state.product.inStock ? var1 : var2;
+  }
+
   render() {
     return (
-      <Link to={"/product/" + this.state.product.id} className="group p-4 font-raleway hover:shadow-xl hover:scale-105 transition">
+      <Link
+        to={"/product/" + this.state.product.id}
+        className="group p-4 font-raleway hover:shadow-xl hover:scale-105 transition"
+        data-testid={"product-" + this.state.product.id}>
         <div className="relative">
-          <img className={"h-[330px] mx-auto mb-6" + (this.state.product.inStock ? "" : " opacity-50")} src={this.state.product.gallery[0]} alt={this.state.product.name + " Product image"} />
+          <img
+            className={
+              "h-[330px] mx-auto mb-6 " +
+              this.checkStock("", "opacity-50")
+            }
+            src={this.state.product.gallery[0]}
+            alt={this.state.product.name + " Product image"}
+          />
 
-          <h1 className={"uppercase text-2xl text-nowrap text-slate-500 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 " + (this.state.product.inStock ? "hidden" : " block")}>out of stock</h1>
+          <h1
+            className={
+              "uppercase text-2xl text-nowrap text-slate-500 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 " +
+              this.checkStock("hidden", "block")
+            }>
+            out of stock
+          </h1>
 
-          <button className="hidden group-hover:block bg-[#5ECE7B] p-4 rounded-full absolute bottom-[-30px] right-6">
+          <button
+            className={
+              "hidden bg-[#5ECE7B] p-4 rounded-full absolute bottom-[-30px] right-6 " +
+              this.checkStock("group-hover:block", "")
+            }
+            onClick={() => {console.log("Fast Add to cart: " + this.state.product.id)}}>
             <img src="/fast_order.svg" alt="Fast order icon" />
           </button>
         </div>
 
         <h1 className="text-xl font-[300]">{this.state.product.name}</h1>
-        <p  className={"text-xl font-[400] mb-4 " + (this.state.product.inStock ? "" : "text-slate-400")}>
+        <p
+          className={
+            "text-xl font-[400] mb-4 " +
+              this.checkStock("", "text-slate-400")
+          }>
           {this.state.product.prices[0].currency.symbol}
           {this.state.product.prices[0].amount}
         </p>
-
       </Link>
     );
   }
--- client/src/components/Products/ProductDetailsPage.jsx
+++ client/src/components/Products/ProductDetailsPage.jsx
@@ -1,15 +1,52 @@
-import { Component } from 'react'
+import { Component } from "react";
+import data from "../../temp/__data";
 
 export default class ProductDetailsPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    const productId = window.location.pathname.split("/").pop();
+    this.state = {
+      productId: productId,
+      product: data.data.products.find((product) => product.id == productId),
+      selectedImage: "",
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      selectedImage: this.state.product.gallery[0],
+    })
+  }
+
+  onImageChangeHandler = (e) => {
+    this.setState({ selectedImage: e.target.src })
+  }
+
   render() {
     return (
-      <div>
-        <h1>Product Details Page</h1>
+      <div className="m-24 font-raleway flex">
+        {/* Product Image Previews and Carousel */}
+        <div className="grid grid-cols-2 gap-6">
+          <div className="columns-1">
+            {this.state.product.gallery.map((image) => (
+              <img onClick={this.onImageChangeHandler} className="size-16 cursor-pointer" key={image} src={image} alt={this.state.productId + " preview"} />
+            ))}
+          </div>
+
+          <div>
+            <img className="size-64" src={this.state.selectedImage} alt="" />
+          </div>
+        </div>
+
+        {/* Product Details + Place Order Button*/}
+        <div className="ml-32 font-semibold">
+            <h1 className="text-3xl ">{this.state.product.name}</h1>
+            <p>Size: </p>
+            <div className="flex">
+              
+            </div>
+        </div>
       </div>
-    )
+    );
   }
 }
